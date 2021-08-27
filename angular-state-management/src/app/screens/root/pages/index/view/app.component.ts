7242b192804c554eb33b8c0b7061f50617dea1b8
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2021-08-28 03:42:45 +0530

    CONSTANTS ADDED

--- angular-state-management/src/app/screens/root/pages/index/view/app.component.ts
+++ angular-state-management/src/app/screens/root/pages/index/view/app.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
 
+const ROOT_INDEX_HTML = 'app.component.html';
+const ROOT_INDEX_CSS = 'app.component.scss';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: 'app-root-index',
+  templateUrl: ROOT_INDEX_HTML,
+  styleUrls: [ROOT_INDEX_CSS]
 })
 export class AppComponent {
   title = 'angular-state-management';
