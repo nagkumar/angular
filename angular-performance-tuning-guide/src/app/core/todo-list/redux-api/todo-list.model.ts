686b9d90907e30cee6a5635448c8d1764ecbd4b2
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2021-11-18 21:14:58 +0530

    removed perf..

--- angular-performance-tuning-guide/src/app/core/todo-list/redux-api/todo-list.model.ts
+++
@@ -1,8 +0,0 @@
-import { TODOItem } from '@app/shared/models/todo-item';
-
-export interface TodoListState {
-  todos: TODOItem[];
-  errors?: Error;
-  isLoading: boolean;
-  editTodoItemIdx?: number;
-}
